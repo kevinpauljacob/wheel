36096f3897f0b8ac1756ad26628388f4f9fc820b
jxhnsebastian <john.babysebastian@gmail.com>
2024-09-18 08:00:36 +0530

    layout

---
+++ app/components/ConnectWallet.tsx
@@ -0,0 +1,37 @@
+import { useWalletModal } from "@solana/wallet-adapter-react-ui";
+import { useWallet } from "@solana/wallet-adapter-react";
+import Image from "next/legacy/image";
+
+export default function ConnectWallet() {
+  const wallet = useWallet();
+  const walletModal = useWalletModal();
+
+  async function logout() {
+    console.log("logging out");
+    try {
+      await wallet.disconnect();
+    } catch (e) {}
+  }
+
+  return (
+    <div className="bg-black border-white/50 border cursor-pointer p-2 rounded">
+      {wallet.publicKey && (
+        <strong className="text-white flex flex-1">
+          {wallet.publicKey.toBase58()}
+        </strong>
+      )}
+
+      {(!wallet.publicKey || wallet.connecting) && (
+        <div
+          onClick={() => {
+            if (!wallet.connected) {
+              walletModal.setVisible(true);
+            }
+          }}
+        >
+          <span>CONNECT WALLET</span>
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ app/providers/providers.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+const endpoint = process.env.NEXT_PUBLIC_RPC!;
+
+const wallets = [
+  new PhantomWalletAdapter(),
+  new SolflareWalletAdapter(),
+  new LedgerWalletAdapter(),
+];
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  console.log(endpoint);
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
